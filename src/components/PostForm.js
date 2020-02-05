08082d939b1d8a6e6f7a0f75998cfacaa6c9da6b
eshoiza <engrjayt200@gmail.com>
2020-02-05 05:22:53 +0100

    added a state to postform component

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
  class PostForm extends Component {
+constructor(props){
+    super(props)
+    this.state = {
+        title: '',
+        body: ''
+    }
+}
+
     render() {
         return (
             <div>
@@ -9,13 +17,13 @@ import React, { Component } from 'react';
                     <div>
                         <label>Title:</label>
                         <br />
-                        <input type="text" name="title"></input>
+                        <input type="text" name="title" value={this.state.title}></input>
                     </div>
                     <br />
                     <div>
                         <label>Body:</label>
                         <br />
-                        <textarea name="body"></textarea>
+                        <textarea name="body" value={this.state.body}></textarea>
                     </div>
                     <br />
                 </form>
