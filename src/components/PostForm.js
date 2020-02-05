c4aec2d6e559f27655c2adeccb506abca1162d64
eshoiza <engrjayt200@gmail.com>
2020-02-05 05:37:18 +0100

    added an Onchange event

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -7,28 +7,42 @@ constructor(props){
         title: '',
         body: ''
     }
+    this.onChange = this.onChange.bind(this);
+}
+
+onChange(e){
+this.setState({[e.target.name]: e.target.value});
 }
 
     render() {
         return (
-            <div>
-                <h1>Add Post</h1>
-                <form>
-                    <div>
-                        <label>Title:</label>
-                        <br />
-                        <input type="text" name="title" value={this.state.title}></input>
-                    </div>
-                    <br />
-                    <div>
-                        <label>Body:</label>
-                        <br />
-                        <textarea name="body" value={this.state.body}></textarea>
-                    </div>
-                    <br />
-                </form>
-            </div>
-        )
+          <div>
+            <h1>Add Post</h1>
+            <form>
+              <div>
+                <label>Title:</label>
+                <br />
+                <input
+                  type="text"
+                  name="title"
+                  onChange={this.state.onChange}
+                  value={this.state.title}
+                ></input>
+              </div>
+              <br />
+              <div>
+                <label>Body:</label>
+                <br />
+                <textarea
+                  name="body"
+                  onChange={this.state.onChange}
+                  value={this.state.body}
+                ></textarea>
+              </div>
+              <br />
+            </form>
+          </div>
+        );
     }
 }
 export default PostForm;
